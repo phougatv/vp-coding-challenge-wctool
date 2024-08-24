b0163e73c4349de81e219cfcf2031bb3f1e83548
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2024-08-24 14:58:35 +0530

    handles "-c" command

--- src/WCTool/Program.cs
+++ src/WCTool/Program.cs
@@ -2,8 +2,29 @@
 
 public class Program
 {
+	private const String Directory = @".\Files";
+
 	static void Main(String[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		if (args.Length != 2)
+		{
+			Console.WriteLine("Incorrect command format, -<command> <filename.extension>");
+			return;
+		}
+
+		var command = args[0];
+		var filename = args[1];
+		var filepath = Path.Combine(Directory, filename);
+		if (!Path.Exists(filepath))
+		{
+			Console.WriteLine($"File: {filename}, not found");
+			return;
+		}
+
+		if (command == "-c")
+		{
+			var fileInfo = new FileInfo(filepath);
+			Console.WriteLine($"{fileInfo.Length} {filename}");
+		}
 	}
 }

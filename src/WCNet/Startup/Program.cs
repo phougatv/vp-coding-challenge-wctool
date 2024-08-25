6ac149d9a46e6a5be7056359693976c48afb5b5c
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2024-08-25 23:03:55 +0530

    added service collection extensions

---
+++ src/WCNet/Startup/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace VP.CodingChallenge.WCNet.Chassis;
+
+using Microsoft.Extensions.DependencyInjection;
+using VP.CodingChallenge.WCNet.CommandHandlers;
+using VP.CodingChallenge.WCNet.Startup;
+
+public class Program
+{
+	private static readonly IServiceCollection ServiceCollection = new ServiceCollection();
+
+	static void Main(String[] args)
+	{
+		//WC service provider
+		var serviceProvider = ServiceCollection.BuildWcNetServiceProvider();
+
+		//Execute DefaultCommandHandler.Handle
+		var commandHandler = serviceProvider.GetRequiredService<DefaultCommandHandler>();
+		commandHandler.Handle(args);
+	}
+}
---
+++ src/WCNet/Startup/ServiceCollectionExtension.cs
@@ -0,0 +1,56 @@
+﻿namespace VP.CodingChallenge.WCNet.Startup;
+
+using Microsoft.Extensions.DependencyInjection;
+using VP.CodingChallenge.WCNet.CommandHandlers;
+using VP.CodingChallenge.WCNet.CommandResolvers;
+using VP.CodingChallenge.WCNet.Commands;
+using VP.CodingChallenge.WCNet.Commands.Concrete;
+
+internal static class ServiceCollectionExtension
+{
+	internal static IServiceProvider BuildWcNetServiceProvider(this IServiceCollection services)
+		=> services.AddWcNet().BuildServiceProvider();
+
+	private static IServiceCollection AddWcNet(this IServiceCollection services)
+	{
+		services
+			.AddWcNetCommands()
+			.AddWcNetCommandResolver()
+			.AddWcNetCommandHandler();
+
+		return services;
+	}
+
+	private static IServiceCollection AddWcNetCommands(this IServiceCollection services)
+	{
+		services
+			.AddKeyedScoped<ICommand, ByteCountCommand>("-c")
+			.AddKeyedScoped<ICommand, LineCountCommand>("-l");
+
+		return services;
+	}
+
+	private static IServiceCollection AddWcNetCommandResolver(this IServiceCollection services)
+	{
+		services
+			.AddScoped<ICommandResolver, DefaultCommandResolver>(provider =>
+			{
+				var keys = new[] { "-c", "-l" }; // Extend this as needed or use reflection to automate
+				var commandMap = keys
+				.ToDictionary(
+					key => key,
+					key => provider.GetRequiredKeyedService<ICommand>(key));
+
+				return new DefaultCommandResolver(commandMap);
+			});
+
+		return services;
+	}
+
+	private static IServiceCollection AddWcNetCommandHandler(this IServiceCollection services)
+	{
+		services.AddScoped<DefaultCommandHandler>();
+
+		return services;
+	}
+}

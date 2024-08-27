92d32961b9d552cf4764ddc1a259e0850c4cb92e
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2024-08-27 10:33:17 +0530

    added unit-test cases

--- src/WCNet/Commands/ICommand.cs
+++ src/WCNet/Commands/ICommand.cs
@@ -1,4 +1,7 @@
-﻿namespace VP.CodingChallenge.WCNet.Commands;
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("VP.CodingChallenge.WCNet.Test")]
+namespace VP.CodingChallenge.WCNet.Commands;
 
 internal interface ICommand
 {

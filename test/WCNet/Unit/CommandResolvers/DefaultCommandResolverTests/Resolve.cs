1b9c37144f1329ea93e4d6ad2df561e79acce660
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2024-08-27 10:57:10 +0530

    added unit-test cases (#9)

---
+++ test/WCNet/Unit/CommandResolvers/DefaultCommandResolverTests/Resolve.cs
@@ -0,0 +1,40 @@
+﻿namespace VP.CodingChallenge.WCNet.Test.Unit.CommandResolvers.DefaultCommandResolverTests;
+
+public class Resolve
+{
+	[Fact]
+	public void ReturnsCommandNotFoundInstance_WhenKeyDoesNotExists()
+	{
+		//Arrange
+		var commandMap = new Dictionary<String, ICommand>
+		{
+			{ "-c", new ByteCountCommand() }
+		};
+		var key = "-w";
+		var defaultResolver = new DefaultCommandResolver(commandMap);
+
+		//Act
+		var actualCommand = defaultResolver.Resolve(key);
+
+		//Assert
+		actualCommand.Should().BeOfType<CommandNotFound>();
+	}
+
+	[Fact]
+	public void ReturnsCommandFromTheDictionary_WhenKeyExists()
+	{
+		//Arrange
+		var commandMap = new Dictionary<String, ICommand>
+		{
+			{ "-c", new ByteCountCommand() }
+		};
+		var key = "-c";
+		var defaultResolver = new DefaultCommandResolver(commandMap);
+
+		//Act
+		var actualCommand = defaultResolver.Resolve(key);
+
+		//Assert
+		actualCommand.Should().BeOfType<ByteCountCommand>();
+	}
+}
---
+++ test/WCNet/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using FluentAssertions;
+global using VP.CodingChallenge.WCNet.CommandResolvers;
+global using VP.CodingChallenge.WCNet.Commands;
+global using VP.CodingChallenge.WCNet.Commands.Concrete;

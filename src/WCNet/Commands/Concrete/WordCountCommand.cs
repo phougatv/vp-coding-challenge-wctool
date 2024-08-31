83ef510501f5aefd7f013ccbba47b94da5f8ddb1
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2024-08-31 09:54:41 +0530

    added word count command

---
+++ src/WCNet/Commands/Concrete/WordCountCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace VP.CodingChallenge.WCNet.Commands.Concrete;
+
+[CommandKey("w")]
+internal class WordCountCommand : ICommand
+{
+    public String Execute(String filepath)
+    {
+        var content = File.ReadAllText(filepath);
+        var pattern = @"\b\w+\b";
+        var matchCollection = Regex.Matches(content, pattern);
+
+        return $"{matchCollection.Count} {Path.GetFileName(filepath)}";
+    }
+}

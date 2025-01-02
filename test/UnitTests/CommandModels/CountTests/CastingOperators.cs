83a4e05f2ecca95a428cdb93a191ace7ce3b563d
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2025-01-02 14:37:38 +0530

    Make Count struct public and add comprehensive tests (#48)
    The `Count` struct in `Count.cs` was modified to change its visibility from `internal` to `public`, allowing access from other assemblies. The `Value` property was also changed from `internal` to `public`.
    
    The `GetHashCode` method in the `Count` struct was updated to use `HashCode.Combine` instead of `Value.GetHashCode`. The `ToString` method was updated to return a formatted string: `Count=[Value: "{Value}"]`. The `!=` operator was modified to use the `==` operator for comparison.
    
    New test classes were added:
    - `CastingOperators` in `CastingOperators.cs` to test implicit conversions between `Count` and various integer types.
    - `Equals` in `Equals.cs` to test the `Equals` method and equality operators (`==` and `!=`) of the `Count` struct.
    - `GetHashCode` in `GetHashCode.cs` to test the `GetHashCode` method of the `Count` struct.
    - `ToString` in `ToString.cs` to test the `ToString` method of the `Count` struct.
    
    The `VP.CodingChallenge.WCNet.UnitTest.csproj` file was modified to remove an unnecessary folder inclusion for `CommandModels\CountTests\`.

---
+++ test/UnitTests/CommandModels/CountTests/CastingOperators.cs
@@ -0,0 +1,85 @@
+﻿namespace VP.CodingChallenge.WCNet.UnitTest.CommandModels.CountTests;
+
+public class CastingOperators
+{
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Int64.MinValue)]
+	[InlineData(Int64.MaxValue)]
+	public void Successfully_ConvertsInt64ToCount(Int64 expected)
+	{
+		//Arrange
+		Count count = expected;
+
+		//Act
+		var actual = count.Value;
+
+		//Assert
+		actual.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Int32.MinValue)]
+	[InlineData(Int32.MaxValue)]
+	public void Successfully_ConvertsInt32ToCount(Int32 expected)
+	{
+		//Arrange
+		Count count = expected;
+
+		//Act
+		var actual = count.Value;
+
+		//Assert
+		actual.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Int16.MinValue)]
+	[InlineData(Int16.MaxValue)]
+	public void Successfully_ConvertsInt16ToCount(Int16 expected)
+	{
+		//Arrange
+		Count count = expected;
+
+		//Act
+		var actual = count.Value;
+
+		//Assert
+		actual.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Byte.MinValue)]
+	[InlineData(Byte.MaxValue)]
+	public void Successfully_ConvertsByteToCount(Byte expected)
+	{
+		//Arrange
+		Count count = expected;
+
+		//Act
+		var actual = count.Value;
+
+		//Assert
+		actual.Should().Be(expected);
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Int64.MinValue)]
+	[InlineData(Int64.MaxValue)]
+	public void Successfully_ConvertsCountToInt64(Int64 int64)
+	{
+		//Arrange
+		var count = new Count(int64);
+		var expected = count.Value;
+
+		//Act
+		Int64 actual = count;
+
+		//Assert
+		actual.Should().Be(expected);
+	}
+}
---
+++ test/UnitTests/CommandModels/CountTests/Equals.cs
@@ -0,0 +1,253 @@
+﻿namespace VP.CodingChallenge.WCNet.UnitTest.CommandModels.CountTests;
+
+public class Equals
+{
+	#region Equals(Count)
+	[Fact]
+	public void ReturnsFalse_WhenNotEqualAndOtherIsDefaultCount()
+	{
+		//Arrange
+		var current = new Count(Int64.MaxValue);
+		var other = default(Count);
+
+		//Act
+		var actual = current.Equals(other);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenNotEqualAndCurrentIsDefaultCount()
+	{
+		//Arrange
+		var current = default(Count);
+		var other = new Count(Int64.MaxValue);
+
+		//Act
+		var actual = current.Equals(other);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenNotEqual()
+	{
+		//Arrange
+		var current = new Count(Int64.MaxValue);
+		var other = new Count(Int64.MinValue);
+
+		//Act
+		var actual = current.Equals(other);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenEqualAndBothHaveDefaultValue()
+	{
+		//Arrange
+		var current = default(Count);
+		var other = default(Count);
+
+		//Act
+		var actual = current.Equals(other);
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(Int64.MinValue)]
+	[InlineData(Int64.MaxValue)]
+	public void ReturnsTrue_WhenEqualAndBothHaveSameValue(Int64 int64)
+	{
+		//Arrange
+		var current = new Count(int64);
+		var other = new Count(int64);
+
+		//Act
+		var actual = current.Equals(other);
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+	#endregion Equals(Count)
+
+	#region Equals(Object)
+	[Fact]
+	public void ReturnsFalse_WhenObjectIsNull()
+	{
+		//Arrange
+		var @this = new Count(Int64.MaxValue);
+		Object? obj = null;
+
+		//Act
+		var actual = @this.Equals(obj);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenObjectIsNotOfTypeCount()
+	{
+		//Arrange
+		var @this = new Count(Int64.MaxValue);
+		Object obj = new CommandKey(Int64.MaxValue.ToString());
+
+		//Act
+		var actual = @this.Equals(obj);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenObjectIsOfTypeCountButIsNotEqual()
+	{
+		//Arrange
+		var @this = new Count(Int64.MaxValue);
+		Object obj = new Count(Int64.MinValue);
+
+		//Act
+		var actual = @this.Equals(obj);
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenObjectIsOfTypeCountAndIsEqual()
+	{
+		//Arrange
+		var @this = new Count(Int64.MaxValue);
+		Object obj = new Count(Int64.MaxValue);
+
+		//Act
+		var actual = @this.Equals(obj);
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+	#endregion Equals(Object)
+
+	#region == Operator
+	[Fact]
+	public void ReturnsFalse_WhenLeftIsNull()
+	{
+		//Arrange
+		Count? left = null;
+		var right = new Count(Int64.MaxValue);
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenRightIsNull()
+	{
+		//Arrange
+		var left = new Count(Int64.MaxValue);
+		Count? right = null;
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsFalse_WhenLeftAndRightAreNotEqual()
+	{
+		//Arrange
+		var left = new Count(Int64.MaxValue);
+		var right = new Count(Int64.MinValue);
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenLeftAndRightAreNull()
+	{
+		//Arrange
+		Count? left = null;
+		Count? right = null;
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenLeftAndRightAreDefault()
+	{
+		//Arrange
+		var left = default(Count);
+		var right = default(Count);
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenLeftAndRightAreEqual()
+	{
+		//Arrange
+		var left = new Count(Int64.MaxValue);
+		var right = new Count(Int64.MaxValue);
+
+		//Act
+		var actual = left == right;
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+	#endregion == Operator
+
+	#region != Operator
+	[Fact]
+	public void ReturnsFalse_WhenLeftAndRightAreEqual()
+	{
+		//Arrange
+		var left = new Count(Int64.MaxValue);
+		var right = new Count(Int64.MaxValue);
+
+		//Act
+		var actual = left != right;
+
+		//Assert
+		actual.Should().BeFalse();
+	}
+
+	[Fact]
+	public void ReturnsTrue_WhenLeftAndRightAreNotEqual()
+	{
+		//Arrange
+		var left = new Count(Int64.MaxValue);
+		var right = new Count(Int64.MinValue);
+
+		//Act
+		var actual = left != right;
+
+		//Assert
+		actual.Should().BeTrue();
+	}
+	#endregion != Operator
+}
